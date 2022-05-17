0d45ee9023f1fc6946d5eb25d6a365fbae2bb996
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-17 22:01:56 +0800

    Added styles to mobile navbar

--- frontend/src/components/Navbar/Mobile/Navbar.jsx
+++ frontend/src/components/Navbar/Mobile/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import logo from '../../../images/logo.svg'
-import styles from './Navbar.module.css'
+import styles from '../Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <img src={logo} alt="Shortly" />
-      <div style={{width: '5rem', height: '5rem', display: 'inline-block'}}>
-        <div style={{width: '100%', height: '1rem', backgroundColor: 'black'}}></div>
-        <div style={{width: '100%', height: '1rem', backgroundColor: 'black', marginTop: '1rem', marginBottom: '1rem'}}></div>
-        <div style={{width: '100%', height: '1rem', backgroundColor: 'black'}}></div>
+      <div className={styles.hamburgerContainer}>
+        <div className={styles.hamburgerLine} />
+        <div className={styles.hamburgerLine} />
+        <div className={styles.hamburgerLine} />
       </div>
     </nav>
   )
