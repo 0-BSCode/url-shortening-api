3988b387cdaf67be0876288d8d86ae1f83510ecf
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-17 20:07:29 +0800

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './images/logo.svg'
 import './App.css';
 
 function App() {
   return (
     <nav>
-      Hi
+      <img src={logo} alt="Shortly" />
     </nav>
   );
 }
