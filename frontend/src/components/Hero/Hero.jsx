1007ec842358d50440ac5b2f8a08a6435215282f
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-18 19:08:07 +0800

    Styled hero

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import illustrationWorking from '../../images/illustration-working.svg'
-
+import styles from './Hero.module.css'
 
 const Hero = () => {
   return (
-    <section>
-      <img src={illustrationWorking} alt="Working" />
-      <div>
-        <h1>
+    <section className={styles.hero}>
+      <img className={styles.hero__img} src={illustrationWorking} alt="Working" />
+      <div className={styles.hero__content}>
+        <h1 className={styles.hero__title}>
           More than just shorter links
         </h1>
-        <p>
+        <p className={styles.hero__subtitle}>
         Build your brand’s recognition and get detailed insights 
         on how your links are performing.
         </p>
-        <button>
+        <button className={styles.hero__btn}>
           Get Started
         </button>
       </div>
