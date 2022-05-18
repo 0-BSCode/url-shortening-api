f38b92b981e436d9913398da6e02b37fe8f6f52f
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-18 19:35:06 +0800

    Styled links form

--- frontend/src/components/Links/Links.jsx
+++ frontend/src/components/Links/Links.jsx
@@ -1,42 +1,50 @@
 import React from 'react'
+import styles from './Links.module.css'
 
 const Links = () => {
   return (
     <section>
-      <form>
-        <input type="text" placeholder="Shorten a link here..." />
-        <button type="submit">
-          Shorten it!
-        </button>
-      </form>
-      <div>
-        <p>
-          https://www.frontendmentor.io
-        </p>
-        <span>
+      <div className={styles.links}>
+        <form className={`${styles.links__form} ${styles.links__cell}`}>
+          <input 
+            className={styles.links__input} type="text" placeholder="Shorten a link here..." />
+          <p
+            className={styles.links__error}>Please add a link
+          </p>
+          <button 
+            className={styles.links__submit} type="submit">
+            Shorten it!
+          </button>
+        </form>
+        <div>
+          <p>
+            https://www.frontendmentor.io
+          </p>
+          <span>
 
-        </span>
-        <p>
-          https://rel.link/k4IKyk
-        </p>
-        <button>
-          Copy
-        </button>
+          </span>
+          <p>
+            https://rel.link/k4IKyk
+          </p>
+          <button>
+            Copy
+          </button>
+        </div>
+        <div>
+          <p>
+            https://www.frontendmentor.io
+          </p>
+          <span>
+
+          </span>
+          <p>
+            https://rel.link/k4IKyk
+          </p>
+          <button>
+            Copied!
+          </button>
+        </div>      
       </div>
-      <div>
-        <p>
-          https://www.frontendmentor.io
-        </p>
-        <span>
-
-        </span>
-        <p>
-          https://rel.link/k4IKyk
-        </p>
-        <button>
-          Copied!
-        </button>
-      </div>      
     </section>
   )
 }
