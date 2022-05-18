3f7a81a7a82986ec6c74e58ba73ee2f8136c8e07
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-18 21:58:32 +0800

    Minor change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,7 +23,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     }
 
     const salt = await bcrypt.genSalt(10)
-    const hasedPass = await bcrypt.hash(password, salt)
+    const hashedPass = await bcrypt.hash(password, salt)
 
     const user = await User.create({
         name,
@@ -33,7 +33,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 
     if (user) {
         res.status(201).json({
-            _id: user.id,
+            _id: user._id,
             name: user.name,
             email: user.email,
             token: generateToken(user._id)
