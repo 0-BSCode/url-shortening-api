5889711603790652b611ab32ba6460e6144a68a0
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-24 20:00:42 +0800

    Styled boost section

--- frontend/src/components/Boost/Boost.jsx
+++ frontend/src/components/Boost/Boost.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import styles from './Boost.module.css'
 
 const Boost = () => {
   return (
-    <section>
-      <h4>
-        Boost your links today
-      </h4>
-      <button>
-        Get Started
-      </button>
+    <section className={styles.boost}>
+      <div className={styles.boost__content}>
+        <h4 className={styles.boost__title}>
+          Boost your links today
+        </h4>
+        <button className={styles.boost__btn}>
+          Get Started
+        </button>
+      </div>
     </section>
   )
 }
