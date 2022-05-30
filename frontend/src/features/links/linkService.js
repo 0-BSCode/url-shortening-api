cad922f2dedb8c37aebbfbb87440121b933fe22b
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-05-30 21:33:23 +0800

    Limited displayed links to 3

--- frontend/src/features/links/linkService.js
+++ frontend/src/features/links/linkService.js
@@ -6,6 +6,7 @@ const getLinks = (initState) => {
 }
 
 const shorten = async (link) => {
+    const linkLimit = 3
     const response = await axios.get(`https://api.shrtco.de/v2/shorten?url=${link}`)
     let links = JSON.parse(localStorage.getItem('links')) || {}
 
@@ -13,6 +14,15 @@ const shorten = async (link) => {
         const newLink = {}
         newLink[link] = response.data.result.full_short_link
         links = Object.assign(newLink, links)
+        
+        if (Object.keys(links).length > linkLimit) {
+            const tempLinks = Object.assign({}, links)
+            links = {}
+            Object.keys(tempLinks).slice(0, linkLimit).map(temp => {
+                links[temp] = tempLinks[temp]
+            })
+        }
+
         localStorage.setItem('links', JSON.stringify(links))
     }
 
