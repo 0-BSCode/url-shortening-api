c93fd8117c0a9b316bb7010868bec52d96810c98
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-06-02 19:18:25 +0800

    Added hover effect for footer buttons

--- frontend/src/components/Footer/Button/Button.jsx
+++ frontend/src/components/Footer/Button/Button.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from '../Footer.module.css'
 
-const Button = ({img, alt}) => {
+const Button = ({img, hov, alt}) => {
   return (
     <button className={styles.footer__btn}>
         <img 
-        className={styles.footer__btnImg} src={img} alt={alt} />
+          className={styles.footer__btnImg} 
+          src={img} 
+          alt={alt}
+          onMouseEnter={e => e.target.src = hov}
+          onMouseOut={e => e.target.src = img}
+        />
     </button>
   )
 }
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import logo from '../../images/logo-white.svg'
 import facebook from '../../images/icon-facebook.svg'
+import facebookHover from '../../images/icon-facebook-hover.svg'
 import twitter from '../../images/icon-twitter.svg'
+import twitterHover from '../../images/icon-twitter-hover.svg'
 import pinterest from '../../images/icon-pinterest.svg'
+import pinterestHover from '../../images/icon-pinterest-hover.svg'
 import instagram from '../../images/icon-instagram.svg'
+import instagramHover from '../../images/icon-instagram-hover.svg'
 import Links from './Links/Links'
 import Button from './Button/Button'
 import styles from './Footer.module.css'
 
 const Footer = () => {
+
   return (
     <section className={styles.footer}>
       <img className={styles.footer__logo} src={logo} alt="Shortly Logo" />
@@ -27,18 +32,22 @@ const Footer = () => {
       <div className={styles.footer__btns}>
         <Button
           img={facebook}
+          hov={facebookHover}
           alt="Facebook"
         /> 
         <Button
           img={twitter}
+          hov={twitterHover}
           alt="Twitter"
         />
         <Button
           img={pinterest}
+          hov={pinterestHover}
           alt="Pinterest"
         />
         <Button
           img={instagram}
+          hov={instagramHover}
           alt="Instagram"
         />                         
       </div>
